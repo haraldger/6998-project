90abc6095759df1a04315de99c397c10d3226cc4
haraldger <harald.gerhardsen@hotmail.com>
2022-12-16 22:54:26 -0500

    Fix loss function

--- swin_agent.py
+++ swin_agent.py
@@ -27,7 +27,7 @@ class SwinAgent:
         self.gamma = gamma
         self.alpha = alpha
         self.batch_size = batch_size
-        self.loss_fn = loss_fn
+        self.loss_fn = loss_fn()
         self.optimizer = torch.optim.Adam(self.Q.parameters(), lr=alpha)
         self.flag=False
         
@@ -55,8 +55,7 @@ class SwinAgent:
         q_values = self.Q(state_batch).gather(1, action_batch)
         target_q_values = self.Q_target(next_state_batch).detach()
         targets = reward_batch + self.gamma * target_q_values.max(1)[0].view(self.batch_size, 1)
-        loss = self.loss_fn()
-        loss(q_values, targets)
+        loss = self.loss_fn(q_values, targets)
 
         # Backpropagation
         self.optimizer.zero_grad()
