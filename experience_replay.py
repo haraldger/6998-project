46b32634313ed29dd3486ba85ac76b4498fc4fed
haraldger <harald.gerhardsen@hotmail.com>
2022-12-17 01:14:27 -0500

    “bugfix”

--- experience_replay.py
+++ experience_replay.py
@@ -11,7 +11,7 @@ class ReplayBuffer:
         self.counter = 0
         self.dims=dims
 
-        self.state_memory = torch.empty(size=[self.capacity, self.dims[0], self.dims[1], self.dims[2]])
+        self.state_memory = torch.empty(size=(self.capacity, self.dims[0], self.dims[1], self.dims[2]))
         self.action_memory = torch.LongTensor((self.capacity,1))
         self.next_state_memory = torch.FloatTensor((self.capacity, self.dims[0], self.dims[1], self.dims[2]))
         self.reward_memory = torch.FloatTensor((self.capacity,1))
