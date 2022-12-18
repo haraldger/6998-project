19d2f29f3d51e30891fff257b56da6ce87f68a06
haraldger <harald.gerhardsen@hotmail.com>
2022-12-17 20:28:50 -0500

    Undo changes

--- pacman.py
+++ pacman.py
@@ -57,11 +57,6 @@ def ms_pacman():
         action = agent.act(previous_state)
         next_state, reward, terminated, truncated, info = env.step(action)
         next_state = process_state(next_state)
-
-        if episode & 1000 == 0:
-            print(episode)
-            plt.imsave('test_frame.png', next_state)
-
         total_reward += reward
 
         # Experience replay
